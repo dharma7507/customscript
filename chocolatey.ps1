ac5d3dbeabe2899996ccd53bcb74eb15868285dc
dharma7507 <dharma548@gmail.com>
2022-10-17 20:34:14 +0530

    Update chocolatey.ps1

--- chocolatey.ps1
+++ chocolatey.ps1
@@ -1,2 +1,106 @@
-Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install notepadplusplus.commandline -f -y
+#Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+#choco install notepadplusplus.commandline -f -y
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.27.0"
+    }
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}          
+}
+
+
+resource "azurerm_resource_group" "myrg1" {
+  name     = "myrg4"
+  location = "West Europe"
+}
+
+resource "azurerm_public_ip" "test" {
+  name                    = "test-pip"
+  location                = azurerm_resource_group.myrg1.location
+  resource_group_name     = azurerm_resource_group.myrg1.name
+  allocation_method       = "Dynamic"
+  idle_timeout_in_minutes = 30
+}
+
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "myvnet-1"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.myrg1.location
+  resource_group_name = azurerm_resource_group.myrg1.name
+}
+
+# Create Subnet
+resource "azurerm_subnet" "mysubnet" {
+  name                 = "mysubnet-1"
+  resource_group_name  = azurerm_resource_group.myrg1.name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_network_interface" "mynic1" {
+  name                = "acctni"
+  location            = azurerm_resource_group.myrg1.location
+  resource_group_name = azurerm_resource_group.myrg1.name
+
+  ip_configuration {
+    name                          = "testconfiguration1"
+    subnet_id                     = azurerm_subnet.mysubnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = "${azurerm_public_ip.test.id}"
+  }
+}
+
+resource "azurerm_windows_virtual_machine" "myvm1" {
+  name                = "mytestvm"
+  resource_group_name = azurerm_resource_group.myrg1.name
+  location            = azurerm_resource_group.myrg1.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  admin_password      = "P@$$w0rd1234!"
+  network_interface_ids = [
+    azurerm_network_interface.mynic1.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2016-Datacenter"
+    version   = "latest"
+  }
+}
+
+resource "azurerm_virtual_machine_extension" "myext1" {
+  name                 = "mytestvm4"
+  virtual_machine_id   = azurerm_windows_virtual_machine.myvm1.id
+  publisher            = "Microsoft.Compute"
+  type                 = "CustomScriptExtension"
+  type_handler_version = "1.9"
+
+  protected_settings = <<PROTECTED_SETTINGS
+    {
+      "commandToExecute": "powershell.exe -Command \"./untitled11.ps1; exit 0;\""
+    }
+  PROTECTED_SETTINGS
+
+  settings = <<SETTINGS
+    {
+        "fileUris": [
+          ""
+        ]
+    }
+  SETTINGS
+
+}
+
